31a7a9e8a4766f29f88bc354e6895136974417a2
shihyuchou621 <kellyu621@gmail.com>
2024-04-11 15:06:40 +0800

    feat: undo delete expense feature

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -44,13 +44,43 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    // 先清除目前所有的SnackBar
+    ScaffoldMessenger.of(context).clearSnackBars();
+    // SnackBar: 短暫顯示是否要回復刪除的訊息
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Expense deleted.'),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registeredExpenses.insert(expenseIndex, expense);
+              });
+            }),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
+    // Scaffold: 一個基本的Material Design布局結構
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter ExpensesTracker'),
@@ -66,10 +96,7 @@ class _ExpensesState extends State<Expenses> {
           const Text('The chart'),
           // 使用Expanded以填滿剩餘空間
           Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
-              onRemoveExpense: _removeExpense,
-            ),
+            child: mainContent,
           ),
         ],
       ),
